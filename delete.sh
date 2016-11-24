fdd440edb3fe6d30e67da0212f95c27fda217b9e
Ruslan Yupyn <r.yupyncrossplusplus.org>
2016-11-24 22:12:04 +0200

    fixed delete script

--- delete.sh
+++ delete.sh
@@ -2,9 +2,11 @@
 
 sleep 5
 
-rm -f "$SRCROOT/Carthage/Build/iOS/9DD57E8B-D1BA-3E64-9952-CE62617CEB7A.bcsymbolmap"
-rm -f "$SRCROOT/Carthage/Build/iOS/23F6E8A6-8D54-3230-8369-776FD4852738.bcsymbolmap"
 rm -rf "$SRCROOT/Carthage/Build/iOS/CrashlyticsCarthage.framework"
 rm -rf "$SRCROOT/Carthage/Build/iOS/CrashlyticsCarthage.framework.DSYM"
+
+grep -r -l "CrashlyticsCarthage" "$SRCROOT/Carthage/Build/iOS" | while read -r file ; do
+    rm -f "$file"
+done
 
 exit 0
